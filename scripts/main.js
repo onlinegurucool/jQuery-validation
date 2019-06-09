aa2fff77e537286a55b59a2e20067fa35d3b6110
Pratap Rockerss <pratap@techcetra.com>
2019-06-09 07:06:23 +0530

    basic validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,23 @@
 $(window).on('load',function(){
     $('.check-form').validate({
-        errorElement: `span`
-    })
-
-    $('[required="required"]').each(function(){
-        $(this).rules("add",{
-            required : true,
-            messages:  {
-                required :function (param,input) {
-                    return `${$(input).prev('label').text()} is required`
-                }
+        errorElement: `span`,
+        rules: {
+            first_name: {
+                required: true,
+                number: true,
+            },
+            last_name: {
+                required: true,
+                number: true,
+            },
+            mobile: {
+                required: true,
+                number: true,
+            },
+            email: {
+                required: true,
+                email: true,
             }
-        })
+        }
     })
-
-    $('[data-validate="email"]').each(function(){
-        $(this).rules("add",{
-            email: true,
-            messages: {
-                email: function (param,input) {
-                    console.log(input)
-                    return `${input.name} is required`
-                }
-            }
-        })
-    })
-
-
 })
