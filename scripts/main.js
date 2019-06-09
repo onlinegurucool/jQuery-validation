67b56e9854d3fe43b24ca8faa4a99ee301c992e0
Pratap Rockerss <pratap@techcetra.com>
2019-06-09 07:45:44 +0530

    mobie number custom validation are done

--- scripts/main.js
+++ scripts/main.js
@@ -26,5 +26,38 @@ $(window).on('load',function(){
         })
     })
 
+    $('[data-validate="mobileNumber"]').each(function(){
+        // custom rules for checking mobile 
+        
+        jQuery.validator.addMethod("mobileBasicValidation",function(value,element){
+            let length = value.length
+            if(length == 10) { // validating for length 10 
+                return true
+            } else if(length == 11 && value[0] == 0) { // validating 11 number and first digit 0
+                return true
+            } else {
+                return false
+            }
+        })
+        $(this).rules("add",{
+            mobileBasicValidation :true,
+            messages: {
+                mobileBasicValidation : function(param,element){
+                    var value = $(element).val()
+                    var length = value.length
+                    if(length < 10) {
+                        return `Mobile number should be min 10 numbers`
+                    } else  if(length == 11 && value[0] != 0) {
+                        return `Invalid Mobile! You might missing first number 0`
+                    } else if(length > 11){
+                        return `Mobile length is too long`
+                    } else {
+                        return `Mobile number is required`
+                    }
+                }
+            }
+        })
+    })
+
 
 })
